dc36a4988df2559c9164eb0b2e8184d4a78b0d6a
DuzyBuzz <wenweng003@gmail.com>
2025-09-02 01:34:03 +0800

    Add project files.

---
+++ Clinic_System/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clinic_System
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+        public static class AppInfo
+        {
+            public static string CurrentVersion = "1.0.0"; // update this when you release new version
+        }
+
+    }
+}
